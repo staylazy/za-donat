28ac74c16281ba99910917f3c5bbfc99179c3e14
staylazy <semenkondratieff@gmail.com>
2021-11-17 22:49:50 +0700

    fix

--- dabase/admin.py
+++ dabase/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import UserProfile
 # Register your models here.
+admin.site.register(UserProfile)
+
+admin.site.site_header="za-donat"
--- dabase/apps.py
+++ dabase/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DabaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dabase'
+
+    def ready(self):
+        import dabase.signals
