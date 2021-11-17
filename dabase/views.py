68c76caf54050865f389ddfb23a253313df27524
staylazy <semenkondratieff@gmail.com>
2021-11-17 20:54:24 +0700

    reg

--- dabase/views.py
+++ dabase/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.generics import (ListCreateAPIView,RetrieveUpdateDestroyAPIView,)
+from rest_framework.permissions import IsAuthenticated
+from .models import UserProfile
+from .permissions import IsOwnerProfileOrReadOnly
+from .serializers import UserProfileSerializer
 
-# Create your views here.
+
+class UserProfileListCreateView(ListCreateAPIView):
+    queryset=UserProfile.objects.all()
+    serializer_class=UserProfileSerializer
+    permission_classes=[IsAuthenticated]
+
+    def perform_create(self, serializer):
+        user=self.request.user
+        serializer.save(user=user)
+
+
+class userProfileDetailView(RetrieveUpdateDestroyAPIView):
+    queryset=UserProfile.objects.all()
+    serializer_class=UserProfileSerializer
+    permission_classes=[IsOwnerProfileOrReadOnly,IsAuthenticated]
