bf822d16bec3cb0d7769d916dc805d0bc0929ace
staylazy <semenkondratieff@gmail.com>
2021-11-23 23:19:21 +0700

    осторожно

--- dabase/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class DabaseConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dabase'
-
-    def ready(self):
-        import dabase.signals
---
+++ t.py
@@ -0,0 +1,12 @@
+import requests
+# try:
+#     token = requests.get("http://127.0.0.1:5000/api/token", auth=('sam', 'pytho')).json()['token']
+# except:
+#     print(requests.get("http://127.0.0.1:5000/api/token", auth=('sam', 'python')).text)
+# print(token)
+# print(requests.get("http://127.0.0.1:5000/api/resource", headers={'Authorization': 'access_token eyJhbGciOiJIUzUxMiIsImlhdCI6MTYzNzU5NjM2MSwiZXhwIjoxNjM3NTk2OTYxfQ.eyJpZCI6MX0.nBRX_fLJYn7hQ8ihb8MgLQj406ReRa_RYd7MtrNEODrO8RWAGGAKMB-HeBt2sWKfxcV6PbKoiE0B00xt4YHziw'}))
+# print(requests.get("http://127.0.0.1:5000/api/resource", auth=('eyJhbGciOiJIUzUxMiIsImlhdCI6MTYzNzU5NjM2MSwiZXhwIjoxNjM3NTk2OTYxfQ.eyJpZCI6MX0.nBRX_fLJYn7hQ8ihb8MgLQj406ReRa_RYd7MtrNEODrO8RWAGGAKMB-HeBt2sWKfxcV6PbKoiE0B00xt4YHziw', 'unused')).text)
+
+
+
+# requests.post("http://127.0.0.1:5000/api/users", json={"username":"sam","password":"python"})
